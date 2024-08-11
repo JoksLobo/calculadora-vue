caf6fb58845a4234a7a35badfbf517511dc8ead9
Joks Lobo <joks@exemplo.com>
2024-08-11 06:35:19 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+
+</script>
+
+<template>
+<div class="container">
+  <h1>Calculadora Ebac</h1>
+  <form>
+    <input type="number"placeholder="Primeiro valor">
+  </form>
+</div>
+
+</template>
+
+<style scoped>
+
+
+</style>
